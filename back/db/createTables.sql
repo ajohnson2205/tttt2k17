e24ad578c512e1dd6e39234739eaaa5c512786b2
Alex Johnson <david.johnsoniv@gmail.com>
2017-11-20 09:52:23 -0700

    adding some sql

--- back/db/createTables.sql
+++ back/db/createTables.sql
@@ -3,51 +3,48 @@ CREATE TABLE snapshots (
   snapshot_status VARCHAR(50),
   user_id INTEGER,
   snapshot_id SERIAL PRIMARY KEY
+);
+
+--NEED TO FIX
+CREATE TABLE events (
+  snapshot_timestamp TIMESTAMP,
+  snapshot_status VARCHAR(50),
+  user_id INTEGER,
+  snapshot_id SERIAL PRIMARY KEY
+);
+
+
+CREATE TABLE statuses (
+  status_added_timestamp TIMESTAMP,
+  status_name VARCHAR(50),
+  status_id SERIAL PRIMARY KEY
+)
+
+
+CREATE TABLE users (
+  --TBD
+  --TBD
+  --TBD
+  user_id SERIAL PRIMARY KEY
 )
 
 
 
 
 
+--POPULATE STATUS DATA
+INSERT INTO statuses (status_name, status_id) VALUES ('none', 0);
+INSERT INTO statuses (status_name) VALUES ('break');
+INSERT INTO statuses (status_name) VALUES ('lunch');
+INSERT INTO statuses (status_name) VALUES ('meeting');
+INSERT INTO statuses (status_name) VALUES ('coaching');
+INSERT INTO statuses (status_name) VALUES ('training');
+INSERT INTO statuses (status_name) VALUES ('ping pong');
+INSERT INTO statuses (status_name) VALUES ('all thumbs');
 
--- EXAMPLES
 
-CREATE TABLE users (
-  firstName VARCHAR(40),
-  lastName VARCHAR(40),
-  genderID INTEGER REFERENCES genders (id),
-  hairColorID INTEGER REFERENCES haircolors (id),
-  eyeColorID INTEGER REFERENCES eyeColors (id),
-  hobbyID INTEGER REFERENCES hobbies (id),
-  picture TEXT,
-  email TEXT,
-  authID TEXT,
-  birthDate DATE,
-  id SERIAL PRIMARY KEY
-);
-
-CREATE TABLE hairColors (
-  hairColor VARCHAR(40),
-  id SERIAL PRIMARY KEY
-);
-
-CREATE TABLE hobbies (
-  hobby VARCHAR(40),
-  id SERIAL PRIMARY KEY
-);
-
-CREATE TABLE eyeColors (
-  eyeColor VARCHAR(40),
-    id SERIAL PRIMARY KEY
-  );
-
-CREATE TABLE genders (
-  gender VARCHAR(6),
-  id SERIAL PRIMARY KEY
-);
-
-CREATE TABLE relationships (
-  id SERIAL PRIMARY KEY,
-  loggedInUserId INTEGER,
-  friendId INTEGER
-);
+--DROP TABLES FOR A NICE RESET
+DROP TABLE snapshots;
+DROP TABLE events;
+DROP TABLE statuses;
+DROP TABLE users;
